2d24f1116892c4ee44584129750c3ebb63a6f6e8
Daniel Mor <danielfalonipe@gmail.com>
2019-03-05 22:07:43 -0500

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,23 @@
+/*============= CLOSE MOBILE NAV ON CLICK =============*/
+
+$(document).ready(function()  {
+   $(document).click(function  (event) {
+     var clickover = $(event.target);
+     var _opened = $(".navbar-collapse").hasClass("show");
+     if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+       $(".navbar-toggler").click();
+     }
+   });
+ });
+ 
+ 
+
+ $(document).ready(function()  {
+  $(window).scroll(function() {
+    if($(this).scrollTop()  > 300)  {
+      $('.navbar').addClass('solid');
+    } else {
+      $('.navbar').removeClass('solid');
+    }
+  });
+});
